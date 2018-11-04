10d564f21ae96df91e94b7c28ef7fc43177711a7
Combos <combos93@mail.ru>
2018-11-05 00:24:45 +0600

    sum but wrong! Need to editing...

--- dice.rb
+++ dice.rb
@@ -1,7 +1,7 @@
 def show_rolling_dice
   100.times do
     print "#{rand(6) + 1}\r"
-    sleep 0.01
+    sleep 0.001
   end
 end
 
@@ -9,9 +9,16 @@ puts "How many dice?"
 num = gets.to_i
 
 num.times do
+  array = []
+  sum = 0
   show_rolling_dice
-  array = rand(6) + 1
-  puts array
   puts rand(6) + 1
+  array = rand(6) + 1
+  sum += array
+  puts  "sum of #{sum}"
 end
 
+
+# array = rand(6) + 1
+#   sum += array
+#   puts  "sum of #{sum}"
